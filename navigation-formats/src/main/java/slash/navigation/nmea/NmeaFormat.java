37f35ac018077005f522615d0928ef15d017c60d
Matjaz Rihtar <matjaz@eunet.si>
2017-10-14 20:03:59 +0200

    Restore my order in NMEA, fix "invalid signature file digest"

--- navigation-formats/src/main/java/slash/navigation/nmea/NmeaFormat.java
+++ navigation-formats/src/main/java/slash/navigation/nmea/NmeaFormat.java
@@ -64,6 +64,18 @@ public class NmeaFormat extends BaseNmeaFormat {
     private static final String MONTH_FORMAT = "MM";
     private static final String YEAR_FORMAT = "yyyy";
 
+    // $GPZDA,032910.542,07,08,2004,00,00*48
+    // $GNZDA,184113.00,23,02,2017,00,00*71
+    private static final Pattern ZDA_PATTERN = Pattern.
+            compile(BEGIN_OF_LINE + "ZDA" + SEPARATOR +
+                    "([\\d\\.]*)" + SEPARATOR + // UTC Time
+                    "(\\d*)" + SEPARATOR +      // day
+                    "(\\d*)" + SEPARATOR +      // month
+                    "(\\d*)" + SEPARATOR +      // year
+                    "\\d*" + SEPARATOR +
+                    "\\d*" +
+                    END_OF_LINE);
+
     // $GPRMC,180114,A,4808.9490,N,00928.9610,E,000.0,000.0,160607,,   ,A*76
     // $GPRMC,140403.000,A,4837.5194,N,00903.4022,E,15.00,0.00,260707,,  *3E
     // $GPRMC,172103.38,V,4424.5358,N,06812.3754,W,0.000,0.000,101010,0,W,N*3A
@@ -137,18 +149,6 @@ public class NmeaFormat extends BaseNmeaFormat {
                     "([^\\*]*)" +
                     "(\\*[0-9A-Fa-f][0-9A-Fa-f])?$");
 
-    // $GPZDA,032910.542,07,08,2004,00,00*48
-    // $GNZDA,184113.00,23,02,2017,00,00*71
-    private static final Pattern ZDA_PATTERN = Pattern.
-            compile(BEGIN_OF_LINE + "ZDA" + SEPARATOR +
-                    "([\\d\\.]*)" + SEPARATOR + // UTC Time
-                    "(\\d*)" + SEPARATOR +      // day
-                    "(\\d*)" + SEPARATOR +      // month
-                    "(\\d*)" + SEPARATOR +      // year
-                    "\\d*" + SEPARATOR +
-                    "\\d*" +
-                    END_OF_LINE);
-
     // $GPVTG,0.00,T,,M,1.531,N,2.835,K,A*37
     // $GPVTG,138.7,T,,M,014.2,N,026.3,K,A*00
     // $GNVTG,251.19,T,,M,0.813,N,1.506,K,D*20
@@ -201,6 +201,10 @@ public class NmeaFormat extends BaseNmeaFormat {
     }
 
     protected boolean isPosition(String line) {
+        Matcher zdaMatcher = ZDA_PATTERN.matcher(line);
+        if (zdaMatcher.matches())
+            return hasValidChecksum(line);
+
         Matcher rmcMatcher = RMC_PATTERN.matcher(line);
         if (rmcMatcher.matches())
             return hasValidChecksum(line) && hasValidFix(line, rmcMatcher.group(9), "N");
@@ -221,10 +225,6 @@ public class NmeaFormat extends BaseNmeaFormat {
         if (wplMatcher.matches())
             return wplMatcher.group(6) == null || hasValidChecksum(line);
 
-        Matcher zdaMatcher = ZDA_PATTERN.matcher(line);
-        if (zdaMatcher.matches())
-            return hasValidChecksum(line);
-
         Matcher vtgMatcher = VTG_PATTERN.matcher(line);
         if (vtgMatcher.matches())
             return hasValidChecksum(line) && hasValidFix(line, vtgMatcher.group(4), "N");
@@ -234,6 +234,16 @@ public class NmeaFormat extends BaseNmeaFormat {
     }
 
     protected NmeaPosition parsePosition(String line) {
+        Matcher zdaMatcher = ZDA_PATTERN.matcher(line);
+        if (zdaMatcher.matches()) {
+            String time = zdaMatcher.group(1);
+            String day = trim(zdaMatcher.group(2));
+            String month = trim(zdaMatcher.group(3));
+            String year = trim(zdaMatcher.group(4));
+            String date = (day != null ? day : "") + (month != null ? month : "") + (year != null ? year : "");
+            return new NmeaPosition(null, null, null, null, null, null, null, parseDateAndTime(date, time), null);
+        }
+
         Matcher rmcMatcher = RMC_PATTERN.matcher(line);
         if (rmcMatcher.matches()) {
             String time = rmcMatcher.group(1);
@@ -306,16 +316,6 @@ public class NmeaFormat extends BaseNmeaFormat {
             String description = wplMatcher.group(5);
             return new NmeaPosition(parseDouble(longitude), westOrEast, parseDouble(latitude), northOrSouth,
                     null, null, null, null, trim(description));
-        }
-
-        Matcher zdaMatcher = ZDA_PATTERN.matcher(line);
-        if (zdaMatcher.matches()) {
-            String time = zdaMatcher.group(1);
-            String day = trim(zdaMatcher.group(2));
-            String month = trim(zdaMatcher.group(3));
-            String year = trim(zdaMatcher.group(4));
-            String date = (day != null ? day : "") + (month != null ? month : "") + (year != null ? year : "");
-            return new NmeaPosition(null, null, null, null, null, null, null, parseDateAndTime(date, time), null);
         }
 
         Matcher vtgMatcher = VTG_PATTERN.matcher(line);
@@ -399,6 +399,22 @@ public class NmeaFormat extends BaseNmeaFormat {
         String altitude = formatAltitude(position.getElevation());
         String speedKnots = position.getSpeed() != null ? formatSpeed(kiloMeterToNauticMiles(position.getSpeed())) : "";
 
+        if(position.hasTime()) {
+            // $GPZDA,032910,07,08,2004,00,00*48
+            String day = formatDay(position.getTime());
+            String month = formatMonth(position.getTime());
+            String year = formatYear(position.getTime());
+            String zda = "GPZDA" + SEPARATOR + time + SEPARATOR + day + SEPARATOR + month + SEPARATOR + year + SEPARATOR + SEPARATOR;
+            writeSentence(writer, zda);
+        }
+
+        // $GPRMC,180114,A,4808.9490,N,00928.9610,E,000.0,000.0,160607,,A*76
+        String rmc = "GPRMC" + SEPARATOR + time + SEPARATOR + "A" + SEPARATOR +
+                latitude + SEPARATOR + northOrSouth + SEPARATOR + longitude + SEPARATOR + westOrEast + SEPARATOR +
+                speedKnots + SEPARATOR + SEPARATOR +
+                date + SEPARATOR + SEPARATOR + "A";
+        writeSentence(writer, rmc);
+
         // $GPGGA,130441.89,5239.3154,N,00907.7011,E,1,08,1.25,16.76,M,46.79,M,,*6D
         String gga = "GPGGA" + SEPARATOR + time + SEPARATOR +
                 latitude + SEPARATOR + northOrSouth + SEPARATOR + longitude + SEPARATOR + westOrEast + SEPARATOR +
@@ -411,22 +427,6 @@ public class NmeaFormat extends BaseNmeaFormat {
                 latitude + SEPARATOR + northOrSouth + SEPARATOR + longitude + SEPARATOR + westOrEast + SEPARATOR +
                 description;
         writeSentence(writer, wpl);
-
-        // $GPRMC,180114,A,4808.9490,N,00928.9610,E,000.0,000.0,160607,,A*76
-        String rmc = "GPRMC" + SEPARATOR + time + SEPARATOR + "A" + SEPARATOR +
-                latitude + SEPARATOR + northOrSouth + SEPARATOR + longitude + SEPARATOR + westOrEast + SEPARATOR +
-                speedKnots + SEPARATOR + SEPARATOR +
-                date + SEPARATOR + SEPARATOR + "A";
-        writeSentence(writer, rmc);
-
-        if(position.hasTime()) {
-            // $GPZDA,032910,07,08,2004,00,00*48
-            String day = formatDay(position.getTime());
-            String month = formatMonth(position.getTime());
-            String year = formatYear(position.getTime());
-            String zda = "GPZDA" + SEPARATOR + time + SEPARATOR + day + SEPARATOR + month + SEPARATOR + year + SEPARATOR + SEPARATOR;
-            writeSentence(writer, zda);
-        }
 
         if(position.getHeading() != null || position.getSpeed() != null) {
             String heading = formatAltitude(position.getHeading());
